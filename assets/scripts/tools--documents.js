4ee4ed4f304eacfad5f785b1e4e295969c7b9c7f
John Peart <code@johnpe.art>
2021-10-10 08:21:27 +0000

    Fix URLs

--- assets/scripts/tools--documents.js
+++ assets/scripts/tools--documents.js
@@ -139,14 +139,14 @@ var base = "https://github.com/civilservicelgbt/documents/upload/main/store"
 	// Open document in new window
 	function openDocumentInWindow() {
 		var documentpath = document.getElementById("details--path");
-		var documentURL = documentpath.innerHTML;
+		var documentURL = base + documentpath.innerHTML;
 		var documentWindow = window.documentWindow = window.open(documentURL, "Open document in new window");
 	}
 
 	// Copy document URL to clipboard
 	function copyDocumentURL() {
 		var documentpath = document.getElementById("details--path");
-		var documentURL = documentpath.innerHTML;
+		var documentURL = base + documentpath.innerHTML;
 		navigator.clipboard.writeText(documentURL);
 		alert("Text copied to clipboard.")
 	}
